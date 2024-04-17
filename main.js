91fd6df16af58dc628689bbaf9f7c399504c5d83
c-atfish <jonlar@student.kristiania.no>
2024-04-17 23:33:29 +0200

    changed material of spinning cube

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -8,9 +8,15 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x4b0082 } );
+const material = new THREE.MeshPhongMaterial( { color:  new THREE.Color("rgb(242,61,93)") } );
 const cube = new THREE.Mesh( geometry, material );
+scene.background = new THREE.Color("rgb(0,61,93)")
+
 scene.add( cube );
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.z = 3
+scene.add(directionalLight)
 
 camera.position.z = 5;
 
@@ -25,13 +31,12 @@ function animate(){
 }
 animate()
 
-renderer.domElement.addEventListener('click', onCanvasClick, false);
-
-function onCanvasClick(event){
-    event.preventDefault();
-    const mouse = new THREE.Vector2(
-        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
-    );
-    
-}
+new FontLoader().load('font.json', (font) => {
+    let textGeo = new TextGeometry(this.settings.text, {
+        font: font,
+        size: 1, // fontsize
+        height: 1, // extrusion
+        curveSegments: 10, // how smooth the text is
+        bevelEnabled: false,
+    });
+})  
